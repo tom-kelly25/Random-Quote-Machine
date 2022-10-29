78e20087cebf914130e362d2f9f9d2b2192c7980
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2022-10-29 18:39:14 +0100

    Implement single author and quote from API

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function App() {
   const [quote, setQuote] = React.useState([]);
-  const [randomQuote, setRandomQuote] = React.useState([]);
-
+  const [randomQuote, setRandomQuote] = React.useState("");
   React.useEffect(() => {
     async function fetchData() {
       const respose = await fetch("https://type.fit/api/quotes");
       const data = await respose.json();
-
       setQuote(data);
       let randIndex = Math.floor(Math.random() * data.length);
       setRandomQuote(data[randIndex]);
@@ -16,9 +14,26 @@ function App() {
 
   return (
     <div>
-      {quote.map((quote) => (
-        <div>{quote.text}</div>
-      ))}
+      <div className="container pt-5">
+        <div className="jumbotron">
+          <div className="card">
+            <div className="card-header">
+              <h2>Inspirational Quotes</h2>
+            </div>
+            <div className="card-body"></div>
+            {randomQuote ? (
+              <>
+                <p className="card-title">
+                  - {randomQuote.author || "No author"}
+                </p>
+                <h5 className="card-text">&quot;{randomQuote.text}&quot;</h5>
+              </>
+            ) : (
+              <h2>Loading</h2>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
