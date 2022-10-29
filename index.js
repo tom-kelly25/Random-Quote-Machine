d1a04dcf7cf9ee9c3ae982a054d6c2ebcf273c7a
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2022-10-29 16:39:41 +0100

    Fetch quotes from API

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 function App() {
-  return <div>Hello World</div>;
+  const [quotes, setQuotes] = React.useState([]);
+  const [randomQuotes, setRandomQuotes] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const respose = await fetch("https://type.fit/api/quotes");
+      const data = await respose.json();
+
+      setQuotes(data);
+      let randIndex = Math.floor(Math.random() * data.length);
+      setRandomQuotes(data[randIndex]);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      Hello World
+      {quotes.map((quote) => (
+        <div>{quote.text}</div>
+      ))}
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
